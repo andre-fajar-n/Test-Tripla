4ba4b907ee2d49d1216cc264fb65894289c3b0a3
andre-fajar-n <andrenugroho395@gmail.com>
2025-08-06 05:44:32 +0700

    chore: use double quote instead of single quote

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,20 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.configure do |config|
-  config.swagger_root = Rails.root.join('swagger').to_s
+  config.swagger_root = Rails.root.join("swagger").to_s
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
-      openapi: '3.0.1',
+    "v1/swagger.yaml" => {
+      openapi: "3.0.1",
       info: {
-        title: 'Test Tripla API V1',
-        version: 'v1',
-        description: 'API documentation for Test Tripla application'
+        title: "Test Tripla API V1",
+        version: "v1",
+        description: "API documentation for Test Tripla application"
       },
       paths: {},
       servers: [
         {
-          url: 'http://localhost:3000',
-          description: 'Development server'
+          url: "http://localhost:3000",
+          description: "Development server"
         }
       ]
     }
