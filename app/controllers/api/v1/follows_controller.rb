14fefb7b17ab5761b8ba26def2a689167cc74190
andre-fajar-n <andrenugroho395@gmail.com>
2025-08-07 08:58:07 +0700

    fix: change logic to handle error 404 in follows controller

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -3,16 +3,18 @@ module Api
     class FollowsController < ApplicationController
       include CurrentUser
 
+      before_action :check_filtered_followed_user, only: %i[create destroy]
+
       # POST /api/v1/follow/:followed_id
       def create
-        followed_user = User.find(params[:followed_id])
+        followed_user = filtered_followed_user
         @current_user.following << followed_user unless @current_user.following.exists?(followed_user.id)
         render json: { message: "Followed successfully" }, status: :created
       end
 
       # DELETE /api/v1/unfollow/:followed_id
       def destroy
-        followed_user = User.find(params[:followed_id])
+        followed_user = filtered_followed_user
         @current_user.following.destroy(followed_user)
         render json: { message: "Unfollowed successfully" }, status: :ok
       end
@@ -27,6 +29,16 @@ module Api
       def following
         following_users = @current_user.following.select(:id, :name, :created_at)
         render json: following_users
+      end
+
+      private
+
+      def filtered_followed_user
+        @filtered_followed_user ||= User.find_by(id: params[:followed_id])
+      end
+
+      def check_filtered_followed_user
+        render json: { error: "User not found" }, status: :not_found and return if filtered_followed_user.nil?
       end
     end
   end
